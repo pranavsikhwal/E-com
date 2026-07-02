734a0cc75ea5de39a2cf4d21821db54827f3dc15
Pranav Sikhwal <pranavsikhwal@gmail.com>
2026-07-02 12:40:09 +0530

    feat: update CORS middleware to include additional allowed origin

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ app = FastAPI()   #it creates backend app
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://e-com-iye7.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
